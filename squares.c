66d48f85238289e19e5f021620b57feb02d7c84d
Kiran <kiran@inteleyes.com>
2025-04-07 12:13:59 +0100

    Wrote a program to calculate and output squares to figure out what structures will be required in the MIPS assembly.

---
+++ squares.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+// calculates the squares of all the numbers between 0 and 200 without a multiply operation
+// Constructs used:
+// Initialising (to zeros) and writing to an array
+// for loop with a loop nested inside. => iterator assigment, compare operations, increment operations.
+
+
+
+int squares[201] = {0};
+
+int main() {
+
+    for (int i = 0; i <= 200; i++)
+    {
+
+        // IF array not initialised to zeros, can run the following here, makes use of this loop
+        // squares[i] = 0;
+
+        // Add i to the current element in the array i(=j) times 
+        for (int j = 0; j < i; j++) {
+            squares[i] = squares[i] + i;
+        }
+    
+        printf("%d \n", squares[i]);
+    }
+
+    return 0;
+}
