1a238a3bf5261787fc9a002cc11d500d877086a6
Kiran <kiran@inteleyes.com>
2025-04-10 23:20:04 +0100

    Intermediate push

--- simulator/main.c
+++ simulator/main.c
@@ -12,7 +12,7 @@ typedef enum {R, I, J} instructionType;
 
 #define ADD 0b00101001
 
-void set_control_unit_signals(struct control_unit *control_unit, uint8_t opcode) {
+void set_control_unit_signals(control_unit *control_unit, uint8_t opcode) {
     // UNIT: Control Unit block
     switch (opcode) {
         case 0b00000000: // R-type instruction
@@ -51,24 +51,24 @@ If chained together correctly and each block is correctly implemented then the w
 
 // register file
 
-struct GPRs {
+typedef struct {
     // short = 2 bytes = 16 bits
     unsigned short r0; // cannot initialise this to 0 
     unsigned short r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18, r19, r20, r21, r22, r23, r24, r25, r26, r27, r28, r29, r30, r31;
-};
+} GPRs;
 
-struct SPRs {
+typedef struct {
     uint8_t PC; // stores the current address in instruction memory
     uint16_t IR; // stores the current instruction
-};
+} SPRs;
 
 // control unit signals
-struct control_unit {
+typedef struct {
     bool RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp1, ALUOp2;
-};
+} control_unit;
 
-struct GPRs REGISTER_FILE = {.r0 = 0};
-struct control_unit CONTROL_UNIT;
+GPRs REGISTER_FILE = {.r0 = 0};
+control_unit CONTROL_UNIT;
 
 bool clock = 0;
 int cycleNumber = 0;
@@ -77,9 +77,9 @@ int cycleNumber = 0;
 
 enum STATE {IF, ID, EX, MEM, WB};
 
-struct instructionState {
+typedef struct {
     enum state;
-};
+} instructionState;
 
 void ASM(state) {
     switch (state)
@@ -87,7 +87,14 @@ void ASM(state) {
     case IF:
         /* code */
         break;
-    
+    case ID:
+        break;
+    case EX:
+        break;
+    case MEM:
+        break;
+    case WB:
+        break;
     default:
         break;
     }
